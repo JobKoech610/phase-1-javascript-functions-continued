7283d3f8438409ff79c7598f5fd0e26301ce0cce
Job koech <jobkoech610@gmail.com>
2023-03-15 15:31:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+const mondayWork = function(activity="go to the office"){
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork();
+mondayWork('work from home');
+
+const saturdayFun = function (activity='roller-skate'){
+return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun('bathe my dog!');
+
+// const wrapAdjective = function (sign ='*', activity = "special"){
+// return (`You are ${sign}${activity}${sign}!`)
+// }
+// wrapAdjective();
+// wrapAdjective("||","a dedicated programmer");
+
+function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special") {
+      return `You are ${visualFlair }${adjective}${visualFlair }!`;
+    }
+  }
+
+ wrapAdjective()();
+ wrapAdjective("||")("a dedicated programmer");
+
+
+  
+
+
